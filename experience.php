abb98b7da6ec6e65f66ebad62b5484f8a7d28ba8
windyayupradini <149036199+windyayupradini@users.noreply.github.com>
2025-01-10 18:07:41 +0700

    experience.php

---
+++ experience.php
@@ -0,0 +1,88 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="experience.css">
+    <title>Portfolio Website</title>
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">SevenGroup</a>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="service.php">Services</a>
+            <a href="skill.php">Skills</a>
+            <a href="#" class="active">Experience</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    </header>
+
+    <div class="container">
+        <div class="experience-section">
+            <h1><span class="highlight">Our</span> Experience</h1>
+            <hr>
+        </div>
+            <div class="experience-item">
+                <h2><span class="highlight">Adobe Premiere</span></h2>
+                <p>Video Editing</p>
+            </div>
+            <div class="home-img">
+                <img src="img/adobep1.png" alt="Image 1">
+                <img src="img/adobep2.png" alt="Image 2">
+                <img src="img/adobep3.png" alt="Image 3">
+            </div>
+            <div class="experience-item">
+                <h2><span class="highlight">Adobe After Effects</span></h2>
+                <p>Animation Editing</p>
+            </div>
+            <div class="home-img">
+                <img src="img/adobeae2.png" alt="Image 1">
+                <img src="img/adobeae3.png" alt="Image 2">
+                <img src="img/adobeae1.jpg" alt="Image 3">
+            </div>
+            <div class="experience-item">
+                <h2><span class="highlight">Adobe Photoshop</span></h2>
+                <p>Photo Editing</p>
+            </div>
+            <div class="home-img">
+                <img src="img/adobephoto1.jpg" alt="Image 1">
+                <img src="img/adobephoto2.jpg" alt="Image 2">
+                <img src="img/adobephoto3.jpg" alt="Image 3">
+            </div>
+            <div class="experience-item">
+                <h2><span class="highlight">Adobe Lightroom</span></h2>
+                <p>Photo Editing</p>
+            </div>
+            <div class="home-img">
+                <img src="img/lightroom.jpg" alt="Image 1">
+                <img src="img/lightroom2.jpg" alt="Image 2">
+                <img src="img/lightroom3.jpg" alt="Image 3">
+            </div>
+            <div class="experience-item">
+                <h2><span class="highlight">Capcut</span></h2>
+                <p>Video Editing</p>
+            </div>
+            <div class="home-img">
+                <img src="img/capcut1.jpg" alt="Image 1">
+                <img src="img/capcut2.jpg" alt="Image 2">
+                <img src="img/capcut3.jpg" alt="Image 3">
+            </div>
+            <div class="experience-item">
+                <h2><span class="highlight">Canva</span></h2>
+                <p>Photo Editing</p>
+            </div>
+            <div class="home-img">
+                <img src="img/canva.jpg" alt="Image 1">
+                <img src="img/canva2.jpg" alt="Image 2">
+                <img src="img/canva3.jpg" alt="Image 3">
+            </div>
+        </section>
+    </div>
+    
+</body>
+</html>
