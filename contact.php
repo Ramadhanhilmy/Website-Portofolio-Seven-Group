79f3af5236b774bb12a9bf6c659aaed8d5461cf4
23091397007 Dandy Satriawan Wicaksono <149364920+DandySatriawanWicaksono@users.noreply.github.com>
2025-01-10 18:42:24 +0700

    contact.php

---
+++ contact.php
@@ -0,0 +1,42 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio Website</title>
+    <link rel="stylesheet" href="contact.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">SevenGroup</a>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="service.php">Services</a>
+            <a href="skill.php">Skills</a>
+            <a href="experience.php">Experience</a>
+            <a href="#" class="active">Contact</a>
+        </nav>
+    </header>
+
+    <div class="container">
+        <div class="contact-form">
+            <h2>Contact Us :</h2>
+            <div class="social-links">
+                <a href="https://www.instagram.com/ramadhly_" target="_blank">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://wa.me/0882009840297" target="_blank">
+                    <i class="fab fa-whatsapp"></i>
+                </a>
+                <a href="https://sevengroup9726@gmail.com" target="_blank">
+                    <i class="fas fa-envelope"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
