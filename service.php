741249ca257bf481abb13b74c70424f34b69fb12
Ramadhanhilmy <149141770+Ramadhanhilmy@users.noreply.github.com>
2025-01-10 17:58:19 +0700

    service.php

---
+++ service.php
@@ -0,0 +1,55 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="service.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> 
+    <title>Portfolio Website</title>
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">SevenGroup</a>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="#" class="active">Services</a>
+            <a href="skill.php">Skills</a>
+            <a href="experience.php">Experience</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    </header>
+
+    <div class="services-section">
+        <h1>Our <span>Services</span></h1>
+        <hr>
+        <div class="services-container">
+            <div class="service-item">
+                <i class="fa-solid fa-video"></i>
+                <p>Videografi</p>
+                <span>Kami menawarkan layanan videografi profesional untuk memenuhi berbagai kebutuhan Anda.</span>
+            </div>
+
+            <div class="service-item">
+                <i class="fas fa-clapperboard"></i>
+                <p>Video Editing</p>
+                <span>Setelah merekam video, Tim editing kami siap untuk menyunting dan mengolah video Anda dengan sentuhan kreatif.</span>
+            </div>
+
+            <div class="service-item">
+                <i class="fas fa-camera"></i>
+                <p>HTML And CSS</p>
+                <span>Kami memiliki fotografer yang handal dalam berbagai genre, termasuk portrait, produk, pernikahan, dan acara.</span>
+            </div>
+
+            <div class="service-item">
+                <i class="fas fa-photo-film"></i>
+                <p>Photo Editing</p>
+                <span>Tim kami berpengalaman dalam mengolah foto dengan berbagai teknik editing, mulai dari penyesuaian warna, pencahayaan, hingga retouching.</span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
