f88f5946156e0246eee1d0eed1725fab8e005a47
Ramadhanhilmy <149141770+Ramadhanhilmy@users.noreply.github.com>
2025-01-10 17:55:41 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link rel="stylesheet" href="style.css">
+    <title>Portfolio Website</title>
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">SevenGroup</a>
+
+        <nav>
+            <a href="#" class="active"> Home</a>
+            <a href="service.php" >Services</a>
+            <a href="skill.php" >Skills</a>
+            <a href="experience.php" >Experience</a>
+            <a href="contact.php" >Contact</a>
+            <a href="logout.php" class="btn-logout">Log Out</a>
+        </nav>
+        
+<!-- <nav>
+<a href="logout.php" class="btn-logout">Log Out</a>
+</naf> -->
+
+    </header>
+    <section class="home">
+        <div class="home-img">
+            <img src="img/fotokel.png" alt="">
+        </div>
+        <div class="home-content">
+            <h1>Hi, It's <span>SevenGroup</span></h1>
+            <h3 class="typing-text">we're <span></span></h3>
+            <p>Selamat datang di situs portofolio SevenGroup, tempat kami menunjukkan karya dan proyek kreatif kami dalam dunia fotografi, videografi, dan editing. Sebagai tim yang terdiri dari para profesional muda, kami memiliki semangat untuk menghasilkan konten visual yang memukau dan berdaya tarik tinggi.
+
+                Di sini, Anda akan menemukan berbagai karya kami yang menampilkan keahlian dalam menggunakan perangkat lunak seperti Adobe Premiere, Adobe After Effects, Photoshop, Lightroom, Capcut, dan Canva. Mulai dari pengeditan video hingga animasi, dari koreksi warna hingga manipulasi foto, kami menawarkan pendekatan inovatif dan hasil berkualitas untuk berbagai kebutuhan visual.</p>
+    </section>
+    
+</body>
+</html>
