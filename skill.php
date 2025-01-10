e1356f37cea2f437ede16fa09cff5c918c89d7c1
windyayupradini <149036199+windyayupradini@users.noreply.github.com>
2025-01-10 18:02:25 +0700

    skill.php

---
+++ skill.php
@@ -0,0 +1,64 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="skill.css">
+    <title>Portfolio Website</title>
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">SevenGroup</a>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="service.php">Services</a>
+            <a href="#" class="active">Skills</a>
+            <a href="experience.php">Experience</a>
+            <a href="contact.php">Contact</a>
+        </nav>
+    </header>
+    
+    <div class="container">
+        <h1>Our <span>Skills</span></h1>
+        <hr>
+        <div class="skills">
+            <div class="skill">
+                <div class="skill-name">Adobe Premiere</div>
+                <div class="skill-bar html"><span></span></div>
+                <div class="skill-percentage">95%</div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">Adobe After Effects</div>
+                <div class="skill-bar css"><span></span></div>
+                <div class="skill-percentage">80%</div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">Adobe Photoshop</div>
+                <div class="skill-bar php"><span></span></div>
+                <div class="skill-percentage">70%</div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">Adobe Lightroom</div>
+                <div class="skill-bar javascript"><span></span></div>
+                <div class="skill-percentage">75%</div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">Capcut</div>
+                <div class="skill-bar angular"><span></span></div>
+                <div class="skill-percentage">100%</div>
+            </div>
+            <div class="skill">
+                <div class="skill-name">Canva</div>
+                <div class="skill-bar wordpress"><span></span></div>
+                <div class="skill-percentage">95%</div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="skill.js"></script>
+</body>
+</html>
